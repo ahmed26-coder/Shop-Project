3c20436cc0984cd2b43d4c2b5a98568f72023907
ahmedadhem <ahmedadhem330@gmail.com>
2025-02-09 16:28:56 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Footer from "./Components/Footer"
-import Navbar from "./components/Navbar";
+import Navbar from "./Components/Navbar";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
